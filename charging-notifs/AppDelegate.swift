8f36771e1299272591b41cb88b7a10ebbc76d046
carolz19 <zhang.carol19@gmail.com>
2020-08-12 00:43:07 -0700

    added popover

--- charging-notifs/AppDelegate.swift
+++ charging-notifs/AppDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Carol Zhang on 2020-08-11.
 //  Copyright © 2020 Carol Zhang. All rights reserved.
-//
+// https://www.raywenderlich.com/450-menus-and-popovers-in-menu-bar-apps-for-macos
 
 import Cocoa
 import AppKit
@@ -15,152 +15,133 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
     let LONGSLEEP = 3600
-
+    let popover = NSPopover()
+    
+    var eventMonitor: EventMonitor?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(togglePopover(_:))
         }
-        constructMenu()
-
+        popover.contentViewController = EditViewController.freshController()
+        
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+            if let strongSelf = self, strongSelf.popover.isShown {
+                strongSelf.closePopover(sender: event)
+            }
+        }
     }
-//
-//
-//    func registerForPushNotifications() {
-//        UNUserNotificationCenter.current() // 1
-//            .requestAuthorization(options: [.alert, .sound, .badge]) { // 2
-//                granted, error in
-//                print("Permission granted: \(granted)") // 3
-//        }
-//    }
+    
+    // action method that will either open or close the popover depending on its current state
+    @objc func togglePopover(_ sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+    
+    // displays the popover to the user. You just need to supply a source rect and macOS will position the popover and arrow so it looks like it’s coming out of the menu bar icon.
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+        
+        eventMonitor?.start()
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
+        eventMonitor?.stop()
+    }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
     
-    func deliverNotification(_ message: String, isCharged: Bool) {
-        let notification = NSUserNotification()
-        notification.title = "\(message)"
-        notification.soundName = NSUserNotificationDefaultSoundName
-        NSUserNotificationCenter.default.scheduleNotification(notification)
-
-    }
-
-    @objc func fortyFiveSeventyFive(_ sender: Any?) {
-        var deliverCount = 0
-        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { timer in
-            let internalFinder = InternalFinder()
-            if let internalBattery = internalFinder.getInternalBattery() {
-                print(internalBattery.charge)
-                // Internal battery found, access properties here.
-                
-                if ((internalBattery.charge ?? .nan) >= 75) {
-                    
-                    // if user is still charging, deliver one more notification
-                    print(internalBattery.isCharging ?? false)
-                    if ((internalBattery.isCharging ?? false) == true) {
-                        
-                        // don't wanna annoy the user! deliver no more than 2 notifs
-                        if (deliverCount < 1) {
-                            self.deliverNotification("Stop Charging", isCharged: internalBattery.isCharging ?? false)
-                            deliverCount += 1
-                            print("number of notifications delivered \(deliverCount)")
-                        }
-                        
-                        // user stopped charging, don't check battery for an hour
-                    } else {
-                        deliverCount = 0
-                        print("user stopped charging")
-                        sleep(UInt32(self.LONGSLEEP))
-                    }
-                    
-                    // tell user to start charging
-                } else if ((internalBattery.charge ?? .nan) <= 45) {
-                    
-                    // if user is NOT charging, deliver some more notifications
-                    if ((internalBattery.isCharging ?? false) == false) {
-                        
-                        // don't wanna annoy the user! deliver no more than 2 notifs
-                        if (deliverCount < 1) {
-                            self.deliverNotification("Start Charging", isCharged: internalBattery.isCharging ?? false)
-                            deliverCount += 1
-                            print("number of notifications delivered \(deliverCount)")
-                        }
-                        
-                        // user started charging, don't need to check battery for a long time
-                    } else {
-                        deliverCount = 0
-                        print("user started charging")
-                        sleep(UInt32(self.LONGSLEEP))
-                    }
-                }
-            }
-            
-        }
-
-    }
-    //
-    @objc func eightyTwenty(_ sender: Any?) {
-        var deliverCount = 0
-        Timer.scheduledTimer(withTimeInterval: 60.0, repeats: true) { timer in
-            let internalFinder = InternalFinder()
-            if let internalBattery = internalFinder.getInternalBattery() {
-            // Internal battery found, access properties here.
-                
-                if ((internalBattery.charge ?? .nan) >= 80) {
-                    
-                    // if user is still charging, deliver one more notification
-                    print(internalBattery.isCharging ?? false)
-                    if ((internalBattery.isCharging ?? false) == true) {
-        
-                        // don't wanna annoy the user! deliver no more than 2 notifs
-                        if (deliverCount < 1) {
-                            self.deliverNotification("Stop Charging", isCharged: internalBattery.isCharging ?? false)
-                            deliverCount += 1
-                            print("number of notifications delivered \(deliverCount)")
-                        }
-
-                    // user stopped charging, don't check battery for an hour
-                    } else {
-                        deliverCount = 0
-                        print("user stopped charging")
-                        sleep(UInt32(self.LONGSLEEP))
-                    }
-                
-                // tell user to start charging
-                } else if ((internalBattery.charge ?? .nan) <= 20) {
-                    
-                    // if user is NOT charging, deliver some more notifications
-                    if ((internalBattery.isCharging ?? false) == false) {
-                        
-                        // don't wanna annoy the user! deliver no more than 2 notifs
-                        if (deliverCount < 1) {
-                            self.deliverNotification("Start Charging", isCharged: internalBattery.isCharging ?? false)
-                            deliverCount += 1
-                            print("number of notifications delivered \(deliverCount)")
-                        }
-                        
-                    // user started charging, don't need to check battery for a long time
-                    } else {
-                        deliverCount = 0
-                        print("user started charging")
-                        sleep(UInt32(self.LONGSLEEP))
-                    }
-                }
-            }
-            
-        }
-    }
-    
-    func constructMenu() {
-        let menu = NSMenu()
-        
-        menu.addItem(NSMenuItem(title: "20-80", action: #selector(AppDelegate.eightyTwenty(_:)), keyEquivalent: "P"))
-        menu.addItem(NSMenuItem(title: "45-75", action: #selector(AppDelegate.fortyFiveSeventyFive(_:)), keyEquivalent: "P"))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-        
-        statusItem.menu = menu
-    }
+//    func deliverNotification(_ title: String, message: String, isCharged: Bool) {
+//        let notification = NSUserNotification()
+//        notification.title = "\(title)"
+//        notification.informativeText = message
+//        notification.soundName = NSUserNotificationDefaultSoundName
+//        NSUserNotificationCenter.default.scheduleNotification(notification)
+//
+//    }
+//
+//    fileprivate func fireTimer(_ max: Double, _ min: Double) -> Timer {
+//        return Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { timer in
+//            let internalFinder = InternalFinder()
+//            if let internalBattery = internalFinder.getInternalBattery() {
+//                print(internalBattery.charge)
+//                // Internal battery found, access properties here.
+//
+//                if ((internalBattery.charge ?? .nan) >= max) {
+//
+//                    // if user if charging
+//                    if ((internalBattery.isCharging ?? false) == true) {
+//
+//                        // notify user
+//                        self.deliverNotification("Stop Charging", message: "Battery is greater than \(max)", isCharged: internalBattery.isCharging ?? false)
+//
+//                        // give internalBattery instance some time to update
+//                        sleep(60)
+//
+//                        // user stopped charging, don't check battery for an hour
+//                    } else {
+//                        print("user stopped charging")
+//                        sleep(UInt32(self.LONGSLEEP))
+//                    }
+//
+//                    // tell user to start charging
+//                } else if ((internalBattery.charge ?? .nan) <= min) {
+//
+//                    // if user is NOT charging, deliver some more notifications
+//                    if ((internalBattery.isCharging ?? false) == false) {
+//
+//                        // notify user
+//                        self.deliverNotification("Start Charging", message: "Battery is less than \(min)", isCharged: internalBattery.isCharging ?? false)
+//
+//                        // give internalBattery instance some time to update
+//                        sleep(60)
+//
+//                        // user started charging, don't need to check battery for a long time
+//                    } else {
+//                        print("user started charging")
+//                        sleep(UInt32(self.LONGSLEEP))
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    @objc func fortyFiveSeventyFive(_ sender: Any?) {
+//        fireTimer(75.0, 60.0)
+//    }
+//
+//    @objc func eightyTwenty(_ sender: Any?) {
+//        fireTimer(80.0, 20.0)
+//    }
+//
+//    @objc func startMonitoring(_ sender: Any?) {
+//        // stub
+//    }
+//
+//    @objc func editMaxMin(_ sender: Any?) {
+//        // stub
+//    }
+//
+//    func constructMenu() {
+//        let menu = NSMenu()
+//
+//        menu.addItem(NSMenuItem(title: "20-80", action: #selector(AppDelegate.eightyTwenty(_:)), keyEquivalent: "e"))
+//        menu.addItem(NSMenuItem(title: "45-75", action: #selector(AppDelegate.fortyFiveSeventyFive(_:)), keyEquivalent: "f"))
+//        menu.addItem(NSMenuItem(title: "Start Monitoring Battery", action: #selector(AppDelegate.startMonitoring(_:)), keyEquivalent: "f"))
+//        menu.addItem(NSMenuItem(title: "Edit Max and Min Charge", action: #selector(AppDelegate.editMaxMin(_:)), keyEquivalent: "f"))
+//        menu.addItem(NSMenuItem.separator())
+//        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+//
+//        statusItem.menu = menu
+//    }
 }
 
---
+++ charging-notifs/EditViewController.swift
@@ -0,0 +1,141 @@
+//
+//  EditViewController.swift
+//  charging-notifs
+//
+//  Created by Carol Zhang on 2020-08-11.
+//  Copyright © 2020 Carol Zhang. All rights reserved.
+//
+
+import Cocoa
+
+var max = 75.0
+var min = 45.0
+
+
+
+
+class EditViewController: NSViewController {
+    
+    @IBOutlet var maxLabel: NSTextField!
+    @IBOutlet var minLabel: NSTextField!
+    
+    @IBOutlet var newMax: NSTextField!
+    @IBOutlet var newMin: NSTextField!
+    
+    let LONGSLEEP = 3600
+    
+    var timer: Timer? // may cause issues
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateValues(max: max, min: min)
+        
+    }
+    
+    func updateValues(max: Double, min: Double) {
+        maxLabel.stringValue = String(describing: "Stop charging at \(max)%")
+        minLabel.stringValue = String(describing: "Start charging at \(min)%")
+        
+    }
+}
+
+extension EditViewController {
+    // MARK: Storyboard instantiation
+    static func freshController() -> EditViewController {
+        //1.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        //2.
+        let identifier = NSStoryboard.SceneIdentifier("EditViewController")
+        //3.
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? EditViewController else {
+            fatalError("Why cant i find EditViewController? - Check Main.storyboard")
+        }
+        return viewcontroller
+    }
+}
+
+// MARK: Actions
+
+extension EditViewController {
+    
+    @IBAction func startMonitoring(_ sender: NSButton) {
+        
+        let updatedMax = Double(newMax.stringValue)
+        let updatedMin = Double(newMin.stringValue)
+        
+        if (updatedMax ?? nil != nil) {
+            max = updatedMax ?? .nan
+            updateValues(max: max, min: min)
+        }
+        
+        if (updatedMin ?? nil != nil) {
+            min = updatedMin ?? .nan
+            updateValues(max: max, min: min)
+        }
+        
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stopMonitoring(_ sender: NSButton) {
+        timer?.invalidate()
+        print("timer invalidated")
+    }
+    
+    @IBAction func quit(_ sender: NSButton) {
+        NSApplication.shared.terminate(sender)
+    }
+    
+    func deliverNotification(_ title: String, message: String, isCharged: Bool) {
+        let notification = NSUserNotification()
+        notification.title = "\(title)"
+        notification.informativeText = message
+        notification.soundName = NSUserNotificationDefaultSoundName
+        NSUserNotificationCenter.default.scheduleNotification(notification)
+        
+    }
+    
+    @objc func fireTimer(sender: Any?){
+        let internalFinder = InternalFinder()
+        if let internalBattery = internalFinder.getInternalBattery() {
+            print(internalBattery.charge)
+            // Internal battery found, access properties here.
+            
+            if ((internalBattery.charge ?? .nan) >= max) {
+                
+                // if user if charging
+                if ((internalBattery.isCharging ?? false) == true) {
+                    
+                    // notify user
+                    self.deliverNotification("Stop Charging", message: "Battery is greater than \(max)", isCharged: internalBattery.isCharging ?? false)
+                    
+                    // give internalBattery instance some time to update
+                    sleep(60)
+                    
+                    // user stopped charging, don't check battery for an hour
+                } else {
+                    print("user stopped charging")
+                    sleep(UInt32(self.LONGSLEEP))
+                }
+                
+                // tell user to start charging
+            } else if ((internalBattery.charge ?? .nan) <= min) {
+                
+                // if user is NOT charging, deliver some more notifications
+                if ((internalBattery.isCharging ?? false) == false) {
+                    
+                    // notify user
+                    self.deliverNotification("Start Charging", message: "Battery is less than \(min)", isCharged: internalBattery.isCharging ?? false)
+                    
+                    // give internalBattery instance some time to update
+                    sleep(60)
+                    
+                    // user started charging, don't need to check battery for a long time
+                } else {
+                    print("user started charging")
+                    sleep(UInt32(self.LONGSLEEP))
+                }
+            }
+        }
+    }
+}
