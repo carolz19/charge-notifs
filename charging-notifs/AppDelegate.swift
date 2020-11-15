9c61033f6c2c21e1172af7298b828ee59683d37b
Carol Zhang <czhang@counterpath.com>
2020-11-14 21:49:01 -0800

    cleaning code

--- charging-notifs/AppDelegate.swift
+++ charging-notifs/AppDelegate.swift
@@ -11,18 +11,17 @@ import AppKit
 import UserNotifications
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDelegate {
 
-    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
-    let LONGSLEEP = 3600
-    
-    let menu = NSMenu()
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+        
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
         }
-        constructMenu()
+        
+        constructMenu(statusItem: statusItem)
         
     }
 
@@ -30,39 +29,63 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
     
-    func constructMenu() {
+    func constructMenu(statusItem: NSStatusItem) {
+        let menu = NSMenu()
+        
         menu.addItem(NSMenuItem(title: "Start Monitoring",
                                     action: #selector(AppDelegate.startMonitoring(_:)),
                                     keyEquivalent: "g"))
         menu.addItem(NSMenuItem(title: "Stop Monitoring",
                                     action: #selector(AppDelegate.stopMonitoring(_:)),
                                     keyEquivalent: "s"))
-//        menu.addItem(NSMenuItem(title: "Preferences",
-//        action: Selector?,
-//        keyEquivalent: "p"))
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit",
                                 action: #selector(NSApplication.terminate(_:)),
                                 keyEquivalent: "q"))
-
-
+        
         statusItem.menu = menu
             
     }
     
+//    func getPermissionNotifs() {
+//        let center = UNUserNotificationCenter.current()
+//        center.requestAuthorization(options: [.alert, .badge, .sound]) {granted, error in
+//
+//            if error != nil {
+//                print("user gave permission to deliv notifs")
+//            } else {
+//                print("user didn't give permission to deliv notifs")
+//            }
+//        }
+//    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     var timer: Timer?
     var max = 75.0
-    var min = 65.0
+    var min = 25.0
     
     @objc func startMonitoring(_ sender: Any?) {
+        
+        // invalidate any existing timers
         timer?.invalidate()
-        print("timer invalidated")
-        timer = Timer.scheduledTimer(timeInterval: 300, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        
+        // checks battery level at a given time interval
+        timer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
     }
     
     @objc func stopMonitoring(_ sender: Any?) {
+        
+        // invalidate timer to stop monitoring
         timer?.invalidate()
-        print("timer invalidated")
     }
     
     func deliverNotification(_ title: String, message: String, isCharged: Bool) {
@@ -77,38 +100,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @objc func fireTimer(_ sender: Any?) {
         let internalFinder = InternalFinder()
         
+        // get user's battery information
         if let internalBattery = internalFinder.getInternalBattery() {
-                    print(internalBattery.charge)
-                    // Internal battery found, access properties here.
-                    
-                    if ((internalBattery.charge ?? .nan) >= max) {
-                        
-                        // if user if charging
-                        if ((internalBattery.isCharging ?? false) == true) {
-                            
-                            // notify user
-                            self.deliverNotification("Stop Charging", message: "Battery is greater than \(max)", isCharged: internalBattery.isCharging ?? false)
-                            
-                        } else {
-                            print("user stopped charging")
-                        }
-                        
-                        // tell user to start charging
-                    } else if ((internalBattery.charge ?? .nan) <= min) {
-                        
-                        // if user is NOT charging, deliver some more notifications
-                        if ((internalBattery.isCharging ?? false) == false) {
-                            
-                            // notify user
-                            self.deliverNotification("Start Charging", message: "Battery is less than \(min)", isCharged: internalBattery.isCharging ?? false)
-                            
-                        } else {
-                            print("user started charging")
-                        }
-                    }
-                }
+            // send notif is battery level above max and user is charging
+            if ((internalBattery.charge ?? .nan) >= max
+                && (internalBattery.isCharging ?? false) == true) {
+                self.deliverNotification("Stop Charging",
+                                        message: "Battery is greater than \(max)",
+                                        isCharged: internalBattery.isCharging ?? false)
+            // send notif is battery level below max and user isn't charging
+            } else if ((internalBattery.charge ?? .nan) <= min
+                && ((internalBattery.isCharging ?? false) == false)) {
+                    self.deliverNotification("Start Charging",
+                                            message: "Battery is less than \(min)",
+                                            isCharged: internalBattery.isCharging ?? false)
+            }
+        }
     }
-    
-    
 }
 
